48fd00f148ff1657a1719134315097bbcd9f7650
Wooler-rgb <104826902+Wooler-rgb@users.noreply.github.com>
2023-05-27 02:19:04 +0700

    Coin production added

--- Incremental Game Project/app.js
+++ Incremental Game Project/app.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
     
     var coins = 10;
-    var coinProduction = 0;
+    var coinProduction = 10;
     var coinGen1Price = 10;
     var coinGen1Amount = 0;
+    var tickspeed = 50;
+    var tickdiv = 20;
     var menu = "coin-gen";
     //function getProduction(){
         //return coinGen1Amount;
     //}
+    setInterval(function(){
+        coins += coinProduction / tickdiv;
+        changeStats();
+    }, tickspeed); 
     $("#coin-gen-1").click(function(){
         coinGen1Amount += 1;
         coinProduction = getProduction();
@@ -18,6 +24,10 @@ $(document).ready(function(){
     $("#gens").click(function(){
         menu = switchMenu("coin-gen");
     })
+    function changeStats(){
+        $("#coins").html(Math.round(coins));
+
+    }
     function switchMenu(fmenu){
         
         $("." + menu).css("display", "none");
