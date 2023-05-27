f4603020087335ee6e64a3176c5df85486cb2cee
Wooler-rgb <104826902+Wooler-rgb@users.noreply.github.com>
2023-05-27 14:37:51 +0700

    Progarmminf and stuff

--- Incremental Game Project/app.js
+++ Incremental Game Project/app.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
     
-    var coins = 10;
-    var coinProduction = 10;
+    var coins = 0;
+    var coinProduction = 1;
     var coinGen1Price = 10;
-    var coinGen1Amount = 0;
+    var coinGen1Amount = 1;
     var tickspeed = 50;
     var tickdiv = 20;
     var menu = "coin-gen";
-    //function getProduction(){
-        //return coinGen1Amount;
-    //}
     setInterval(function(){
         coins += coinProduction / tickdiv;
         changeStats();
     }, tickspeed); 
     $("#coin-gen-1").click(function(){
+        coins -= 10;
         coinGen1Amount += 1;
         coinProduction = getProduction();
+        changeStats();
     })
     $("#upg").click(function(){
         menu = switchMenu("upgrades");
@@ -24,14 +23,27 @@ $(document).ready(function(){
     $("#gens").click(function(){
         menu = switchMenu("coin-gen");
     })
+    function getProduction(){
+        return coinGen1Amount;
+    }
     function changeStats(){
         $("#coins").html(Math.round(coins));
-
+        $("#coin-gen-1-num").html("Slaves: " + coinGen1Amount + " ");
+        if(coins < 10)
+            disableBtn("coin-gen-1");
+        else
+            enableBtn("coin-gen-1");
     }
     function switchMenu(fmenu){
         
         $("." + menu).css("display", "none");
         $("." + fmenu).css("display", "block");
         return fmenu;
+    }
+    function disableBtn(btn) {
+        document.getElementById(btn).disabled = true;
+    }
+    function enableBtn(btn) {
+        document.getElementById(btn).disabled = false;
     }
 });
