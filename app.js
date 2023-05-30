de0c24762b84285922afdf3ef0b543efc0d3e7fe
Wooler-rgb <104826902+Wooler-rgb@users.noreply.github.com>
2023-05-30 22:49:33 +0700

    Added indicators

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
     
     var coins = 0;
     var coinProduction = 1;
+    var coffee_mult = 1;
+    var pick_mult = 1;
     var global_multi = 1;
     var coinGenPrice = [10, 100, 1500, 12500, 100000];
+    var coinGenEff = [1, 10, 100, 500, 3000];
     var coinBuffPrice = [1000, 10000];
     var coinGenAmount = [1, 0, 0, 0, 0];
     var coinBuffAmount = [0, 0];
@@ -38,21 +41,22 @@ $(document).ready(function(){
     $("#coin-gen-4").click(function(){
         coins -= coinGenPrice[3];
         coinGenAmount[3] += 1;
-        coinGenPrice[3] *= 1.35;
+        coinGenPrice[3] *= 1.4;
         coinProduction = getProduction();
         changeStats();
     })
     $("#coin-gen-5").click(function(){
         coins -= coinGenPrice[4];
         coinGenAmount[4] += 1;
-        coinGenPrice[4] *= 1.4;
+        coinGenPrice[4] *= 1.5;
         coinProduction = getProduction();
         changeStats();
     })
     $("#coffee").click(function(){
         coins -= coinBuffPrice[0];
         coinBuffAmount[0] += 1;
-        coinBuffPrice[0] *= 2;
+        coinBuffPrice[0] *= 2.5;
+        coffee_mult *= 1.1;
         global_multi *= 1.1;
         coinProduction = getProduction();
         changeStats();
@@ -61,6 +65,7 @@ $(document).ready(function(){
         coins -= coinBuffPrice[1];
         coinBuffAmount[1] += 1;
         coinBuffPrice[1] *= 10;
+        pick_mult *= 2;
         global_multi *= 2;
         coinProduction = getProduction();
         changeStats();
@@ -72,10 +77,11 @@ $(document).ready(function(){
         menu = switchMenu("coin-gen");
     })
     function getProduction(){
-        return (coinGenAmount[0] + (coinGenAmount[1] * 10) + (coinGenAmount[2] * 100) + (coinGenAmount[3] * 500) + (coinGenAmount[4] * 4000)) * global_multi;
+        return (coinGenAmount[0] * coinGenEff[0] + (coinGenAmount[1] * coinGenEff[1]) + (coinGenAmount[2] * coinGenEff[2]) + (coinGenAmount[3] * coinGenEff[3]) + (coinGenAmount[4] * coinGenEff[4])) * global_multi;
     }
     function changeStats(){
-        $("#coins").html(~~coins);
+        $("#coins").html(~~coins + "&nbsp&nbsp&nbsp&nbsp");
+        $("#c-per-s").html(~~coinProduction + " c/s");
         $("#coin-gen-1-num").html("Slaves: " + coinGenAmount[0] + " ");
         $("#coin-gen-2-num").html("Staff: " + coinGenAmount[1] + " ");
         $("#coin-gen-3-num").html("Watchers: " + coinGenAmount[2] + " ");
@@ -89,7 +95,14 @@ $(document).ready(function(){
         $("#coin-gen-4-cost").html("Cost: " + ~~coinGenPrice[3] + " coins");
         $("#coin-gen-5-cost").html("Cost: " + ~~coinGenPrice[4] + " coins");
         $("#coffee-cost").html("Cost: " + ~~coinBuffPrice[0] + " coins");
-        $("#pickaxes-5-cost").html("Cost: " + ~~coinBuffPrice[1] + " coins");
+        $("#pickaxes-cost").html("Cost: " + ~~coinBuffPrice[1] + " coins");
+        $("#coin-gen-1-prod").html(" " + ~~(coinGenAmount[0] * coinGenEff[0] * global_multi) + " c/s");
+        $("#coin-gen-2-prod").html(" " + ~~(coinGenAmount[1] * coinGenEff[1] * global_multi) + " c/s");
+        $("#coin-gen-3-prod").html(" " + ~~(coinGenAmount[2] * coinGenEff[2] * global_multi) + " c/s");
+        $("#coin-gen-4-prod").html(" " + ~~(coinGenAmount[3] * coinGenEff[3] * global_multi) + " c/s");
+        $("#coin-gen-5-prod").html(" " + ~~(coinGenAmount[4] * coinGenEff[4] * global_multi) + " c/s");
+        $("#coffee-eff").html(" " + ~~(coffee_mult * 100) + "% Production, +10% per coffee machine");
+        $("#pickaxes-eff").html(" " + ~~pick_mult + "x Production, Pickaxe Power: 2x");
         if(coins < coinGenPrice[0])
             disableBtn("coin-gen-1");
         else
